a55f770e108b2ee01f3e789ba8b60ec8a36f20e9
guill <gegc@dnkweb.com>
2021-12-14 23:33:46 -0600

    Guillermo, Cambios a la clases TipoProfesor y Turno

--- tipoProfesor.py
+++ tipoProfesor.py
@@ -1,5 +1,22 @@
 from turno import *
 class Tipo_profesor:
+
+    lista_tipo_profesor = []
+
+    
+    #Metodo para registrar un nuevo Tipo de profesor
+    def registrar_tipos_profesor():
+        print("Registro de Tipo de profesor\n")
+        t_profesor = input("Ingrese un tipo de profesor")
+        objtipoProfesor = t_profesor
+
+        Tipo_profesor.lista_tipo_profesor.append(objtipoProfesor)
+
+    def mostrar_tipos_profesor():
+        for i in Tipo_profesor.lista_turnos:
+            print (i)
+
+    
     def __init__(self, tipo_profesor):
         self.__tipo_profesor = tipo_profesor
         self.__turno = Turno()
@@ -11,3 +28,5 @@ class Tipo_profesor:
         self.__tipo_profesor = tipo_profesor
 
     tipo_profesor = property(get_tipo_profesor, set_tipo_profesor)
+
+
--- turno.py
+++ turno.py
@@ -1,9 +1,29 @@
 
 class Turno:
+
+    lista_turno = []
+
     def __init__(self, turno):
         self.__turno = turno
         
 
+    def agregar_turno ():
+        print("Agregar turno \n")
+        turno = input("Ingrese un turno: ")
+        obj_turno = turno
+        Turno.lista_turno.append(obj_turno)
+
+    def eliminar_turno():
+        print("Eliminar Turno\n")
+        trn = input("Ingrese el turno a eliminar")
+        for j in Turno.lista_turno:
+            if trn == j:
+                Turno.lista_turno.remove(j)
+
+    def mostrar_turnos():
+        print("Mostrar Turnos\n")
+        for i in Turno.lista_turno:
+            print(i)
 
     def get_turno(self):
         return self.__turno
