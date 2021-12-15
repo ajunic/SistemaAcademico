9ef6853daa71297b8fcf946cee1c04680e26a814
guill <gegc@dnkweb.com>
2021-12-14 23:43:10 -0600

    Guillermo, Cambios a las clases TipoProfesor y Turno

--- tipoProfesor.py
+++ tipoProfesor.py
@@ -16,6 +16,13 @@ class Tipo_profesor:
         for i in Tipo_profesor.lista_turnos:
             print (i)
 
+    def eliminar_tipo_profesor():
+        print("Eliminar Tipo de profesor\n")
+        t_profesor = input("Ingrese el tipo de profesor a eliminar: ")
+        for i in Tipo_profesor.lista_tipo_profesor:
+            if t_profesor == i:
+                Tipo_profesor.lista_tipo_profesor.remove(i)
+
     
     def __init__(self, tipo_profesor):
         self.__tipo_profesor = tipo_profesor
