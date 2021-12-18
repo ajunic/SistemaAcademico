b243e0d4aeb02734413327842bb70a1eb09c321b
ashly ramos <rivasvashly16@gmail.com>
2021-12-18 01:10:34 -0600

    se completo metodos basicos

--- turno.py
+++ turno.py
@@ -1,9 +1,39 @@
 
 class Turno:
+    listaTurno = []
+
     def __init__(self, turno):
         self.__turno = turno
         
+    def registrar_turno():
+        print("Registrar turno\n")
+        turno = input("Ingrese el nombre del turno: ")
+        objTurno = Turno(turno)
+        Turno.listaTurno.append(objTurno)
 
+    def elimninar_turno():
+        print("Eliminar turno\n")
+        borrar = input("Escriba el nombre del turno que desea eliminar: ")
+        bo = Turno.listaTurno.pop(borrar)
+        print("-------------------")
+        print("------------------------")
+        print("Se ha eliminado con exito")
+    
+    def buscar_turno():
+        print("Buscar turno\n")
+        buscar = input("Escriba el nombre del turno que desea encontrar: ")
+        bu = Turno.listaTurno.index(buscar)
+        print("-------------------")
+        print("Buscando------------------------")
+        print(f"El turno que busca se encuentra en: {bu}")
+    
+    def modificar_turno():
+        print("Modificar turno\n")
+        numi = int(input("Ingrese el indice que desea modificar: "))
+        print(Turno.listaTurno[: numi])
+        modi = input("Ingrese la modificacion que desea realizar: ")
+        Turno.listaTurno[:numi] = [modi]
+        print(Turno.listaTurno)
 
     def get_turno(self):
         return self.__turno
